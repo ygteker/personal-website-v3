60b993bdf1bca549ad7bfeeea5de4ea5bf6481c1
ygteker <gunes.teker@gmail.com>
2025-07-13 16:22:18 +0200

    fix

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,6 @@
 import { ThemeToggle } from "./theme-toggle";
 
-export function SiteHeader(props: any) {
+export function SiteHeader() {
   return (
     <header className="fixed top-0 left-0 w-full px-6 py-4 flex justify-between items-center z-50">
       <div className="text-sm font-medium text-foreground">gunes teker</div>
